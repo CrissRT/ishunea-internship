dfd4697f2c5ee6b47a64895c10ed78abdf8b5af5
Cristian <smartcristian2002@gmail.com>
2024-08-07 10:17:55 +0300

    Add pagination, unoptimized version

--- src/routes/Blog/ListBlogs.js
+++ src/routes/Blog/ListBlogs.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./ListBLogs.css"
 import NewsCards from "../../components/NewsCards"
 
 function ListBlogs({blogs}) {
+    const blogsEachPage = [];
+    for (let i = 0; i < blogs.length; i += 4) {
+        blogsEachPage.push(blogs.slice(i, i + 4));
+    }
+    const [currentPage, setCurrentPage] = useState(0);
+
+    const handlePage = (e) => {
+        if (e.target.id === "left-page" && blogsEachPage[currentPage - 1])
+            setCurrentPage(currentPage - 1)
+        else if (e.target.id === "right-page" && blogsEachPage[currentPage + 1])
+            setCurrentPage(currentPage + 1)
+    }
+
     const column1 = []
     const column2 = []
     const column3 = []
@@ -15,19 +28,22 @@ function ListBlogs({blogs}) {
     let column3Size = sizes[2]
     let column4Size = sizes[0]
     
+    const currentPageContent = blogsEachPage[currentPage];
 
-    for (let i = 0; i < blogs.length; i++) {
+
+    for (let i = 0; i < currentPageContent.length; i++) {
+        const blog = currentPageContent[i]
         if (i % 4 === 0) {
-            column1.push(<NewsCards item={blogs[i]} key={i} height={column1Size}/>)
+            column1.push(<NewsCards item={blog} key={i} height={column1Size}/>)
             column1Size = sizes[(sizes.indexOf(column1Size) + 1) % sizes.length]
         } else if (i % 4 === 1) {
-            column2.push(<NewsCards item={blogs[i]} key={i} height={column2Size}/>)
+            column2.push(<NewsCards item={blog} key={i} height={column2Size}/>)
             column2Size = sizes[(sizes.indexOf(column2Size) + 1) % sizes.length]
         } else if (i % 4 === 2) {
-            column3.push(<NewsCards item={blogs[i]} key={i} height={column3Size}/>)
+            column3.push(<NewsCards item={blog} key={i} height={column3Size}/>)
             column3Size = sizes[(sizes.indexOf(column3Size) + 1) % sizes.length]
         } else {
-            column4.push(<NewsCards item={blogs[i]} key={i} height={column4Size}/>)
+            column4.push(<NewsCards item={blog} key={i} height={column4Size}/>)
             column4Size = sizes[(sizes.indexOf(column4Size) + 1) % sizes.length]
         }
     }
@@ -52,7 +68,13 @@ function ListBlogs({blogs}) {
                 {column4.map((card) => (card))}
             </div>
         </div>
-        {blogs.length > 20 ? <div><div>left</div><div>right</div></div>: <></>}
+        
+        <div className='width-100 d-flex justify-content-center'>
+            <div class="pagination wid">
+                <div id="left-page" onClick={(e) => handlePage(e)}>❮</div>
+                <div id='right-page' onClick={(e) => handlePage(e)}>❯</div>
+            </div>
+        </div>
     </section>
   )
 }
